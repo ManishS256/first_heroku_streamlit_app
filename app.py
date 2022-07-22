827d5d30495372e2067de3ae2d443c53d2ad3df0
ManishS256 <84698392+ManishS256@users.noreply.github.com>
2022-07-22 12:41:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-def calculate(a,b,c):
+'''def calculate(a,b,c):
     maxnumber=max([a,b,c])
-    st.write("""## Max of the given Numbers is """+ str(maxnumber))
+    st.write("""## Max of the given Numbers is """+ str(maxnumber))'''
 st.write("""
 # Finds the max of the given three numbers
 # """)
-a=st.number_input("First Number")
+'''a=st.number_input("First Number")
 b=st.number_input("Second Number")
 c=st.number_input("Third Number")
-st.button("Calculate", on_click=calculate(a,b,c))
+st.button("Calculate", on_click=calculate(a,b,c))'''
